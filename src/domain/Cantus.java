51b57b6d8fbc29cd7e04399944407c3471ac9a58
bartolo05 <vdbroeckb@gmail.com>
2018-01-23 19:00:28 +0100

    HS omgezet in arraylist getters gemaakt

--- src/domain/Cantus.java
+++ src/domain/Cantus.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 
 public class Cantus {
-	public Date datum;
+
+
+    public Date datum;
 	public String vereniging;
 	public String naam;
 	public String plaats;
@@ -22,8 +24,44 @@ public class Cantus {
 		this.naam = naam;
 	}
 
+	public Cantus(String str){
+	    String d = 
+    }
+
 	public String toString(){
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 		return "Cantus: " + naam + " op " + df.format(datum) + " bij " + vereniging;
 	}
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public String getVereniging() {
+        return vereniging;
+    }
+
+    public void setVereniging(String vereniging) {
+        this.vereniging = vereniging;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getPlaats() {
+        return plaats;
+    }
+
+    public void setPlaats(String plaats) {
+        this.plaats = plaats;
+    }
 }
