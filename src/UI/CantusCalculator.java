24cb0a1d51aa62b842c294026829db8b6211d19d
OppositeNote <bartold.vandenplas@telenet.be>
2018-01-23 16:36:05 +0100

    Merge conflict opgelost

--- src/UI/CantusCalculator.java
+++ src/UI/CantusCalculator.java
@@ -8,25 +8,10 @@ import java.nio.file.Paths;
 
 public class CantusCalculator {
 
-<<<<<<< HEAD
 
 	public static void main(String[] args) 
 		{
 			Controller controller = new Controller();
 			controller.init();
 		}
-=======
-	public CantusVerzameling CV;
-public static void main(String[] args) throws FileNotFoundException, IOException{
-	if(args.length == 0){
-        System.out.println("no input was given.");
-        return ;
-	}
-	else{
-    Scanner in = new Scanner(Paths.get(args[0]));
-    in.useDelimiter(",");
-	}
-	Facade f = new Facade();
-}
->>>>>>> branch 'master' of https://github.com/r0372092/CantusCalulator
 }
