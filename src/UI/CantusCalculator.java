aa0258f6ecf4aea20be145835985abc1a9249678
bartolo05 <vdbroeckb@gmail.com>
2018-01-23 15:35:42 +0100

    commit test from intellij

--- src/UI/CantusCalculator.java
+++ src/UI/CantusCalculator.java
@@ -2,17 +2,21 @@ package UI;
 import domain.*;
 import java.io.*;
 import java.util.*;
+import java.io.IOException; 
+import java.nio.file.Paths;
+
 
 public class CantusCalculator {
 
 	public CantusVerzameling CV;
-public static void main(String[] args) throws FileNotFoundException{
+public static void main(String[] args) throws FileNotFoundException, IOException{
 	if(args.length == 0){
         System.out.println("no input was given.");
         return ;
 	}
 	else{
-    Scanner sc = new Scanner(new File(args[0])).useLocale(Locale.US);
+    //Scanner sc = new Scanner(new File(args[0]));
+    Scanner in = new Scanner(Paths.get(args[0]));
     
 	}
 }
