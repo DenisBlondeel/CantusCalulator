b80b697e0d017bfe7c8284f33669736ce5f86610
OppositeNote <bartold.vandenplas@telenet.be>
2018-01-23 16:37:18 +0100

    Nu wel

--- src/domain/CantusVerzameling.java
+++ src/domain/CantusVerzameling.java
@@ -3,9 +3,6 @@ package domain;
 import java.util.HashSet;
 
 public class CantusVerzameling {
-<<<<<<< HEAD
-	public HashSet<Cantus> cantus;
-=======
 	public HashSet<Cantus> hs;
 	
 	CantusVerzameling(){
@@ -14,5 +11,4 @@ public class CantusVerzameling {
 	public void init(){
 
 	}
->>>>>>> branch 'master' of https://github.com/r0372092/CantusCalulator
 }
