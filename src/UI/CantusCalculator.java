399c989af7bae95b3015babed6889e7e07c10843
bartolo05 <vdbroeckb@gmail.com>
2018-01-23 16:50:36 +0100

    facade aangemaakt en scanner werkt

--- src/UI/CantusCalculator.java
+++ src/UI/CantusCalculator.java
@@ -5,21 +5,10 @@ import java.util.*;
 import java.io.IOException; 
 import java.nio.file.Paths;
 
-
-<<<<<<< HEAD
-public class CantusCalculator {
-=======
 public class CantusCalculator {
 
->>>>>>> branch 'master' of https://github.com/r0372092/CantusCalulator
-
-	public static void main(String[] args) 
-		{
-			Controller controller = new Controller();
-			controller.init();
-		}
-<<<<<<< HEAD
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.init();
+    }
 }
-=======
-}
->>>>>>> branch 'master' of https://github.com/r0372092/CantusCalulator
--- src/domain/CantusVerzameling.java
+++ src/domain/CantusVerzameling.java
@@ -1,4 +1,8 @@
 package domain;
+import java.nio.file.FileStore;
+import java.util.*;
+import java.text.*;
+
 
 import java.util.HashSet;
 
@@ -8,7 +12,34 @@ public class CantusVerzameling {
 	CantusVerzameling(){
 		this.hs = new HashSet<>();
 	}
-	public void init(){
+	public void init(Scanner sc){
+	   CantusVerzameling CV = new CantusVerzameling();
+	   String Firstline = sc.nextLine();
+
+	   if(Firstline.toLowerCase().contains("plaats".toLowerCase())){
+           System.out.println("plaats in eerste lijn");
+       }
+       else{
+           System.out.println("plaats niet in eerste lijn");
+           Date datum = dateer(sc.next());
+
+           Cantus cantus = new Cantus(datum,sc.next(),sc.next());
+           System.out.println(cantus.toString());
+       }
 
 	}
+
+	private Date dateer(String s){
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+	    try {
+            Date date = df.parse(s);
+            if(date.getYear()<100)
+                date.setYear(date.getYear()+2000);
+            return date;
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
