a682a8f1c3dc30485717ada7ba0f09862ed2b09b
bartolo05 <vdbroeckb@gmail.com>
2018-01-24 20:35:30 +0100

    eerste grafiek werkt al een heel klein beetje

--- src/UI/MainPane.java
+++ src/UI/MainPane.java
@@ -7,6 +7,19 @@ import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.axis.DateAxis;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+
 public class MainPane extends JFrame implements Observer{
 	
 	private static final long serialVersionUID = -9090407129402452701L;
@@ -25,6 +38,26 @@ public class MainPane extends JFrame implements Observer{
 		frame = new JFrame(); //creates new frame with set dimensions
 		frame.setSize(950, 400);
 		frame.setTitle("Plot");
+		int i = 1;
+		TimeSeriesCollection ds = new TimeSeriesCollection();
+		TimeSeries series = new TimeSeries("Series1");
+		for(Date d: dataset){
+			series.addOrUpdate(new Day(d),i);
+			i++;
+		}
+		ds.addSeries(series);
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(
+				"cantustijdlijn test", // Chart
+				"Date", // X-Axis Label
+				"cantus", // Y-Axis Label
+				ds);
+
+		XYPlot plot = (XYPlot)chart.getPlot();
+		//plot.setBackgroundPaint(new Color(255,228,196));
+
+		ChartPanel panel = new ChartPanel(chart);
+		setContentPane(panel);
+
 		frame.setVisible(true);
 		
 		DateAxis ax;
